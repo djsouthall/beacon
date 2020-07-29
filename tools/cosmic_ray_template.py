47997a53a3a947729dee27eecd7ea4d13cb2ed4b
Dan Southall <dsouthall@uchicago.edu>
2020-07-29 15:43:52 -0500

    Rough CR template coming along.  Want to look at real CR signals to motivate more appropriate shape of CR "bipolar" shape.

--- tools/cosmic_ray_template.py
+++ tools/cosmic_ray_template.py
@@ -24,14 +24,8 @@ datapath = os.environ['BEACON_DATA']
 plt.ion()
 
 
-def loadInterpolatedMeanResponse(mode, upsample_factor=16, plot=False):
+def loadInterpolatedMeanResponse(mode, return_domain='time', upsample_factor=16, plot=False):
     '''
-    THIS IS LEFT OFF TRYING TO GET THE RESPONSES AVERAGED IN THE TIME DOMAIN.  IT IS CLEAR LOOKING AT THE RESPONSES
-    AS CURRENTLY PRESENT THAT THEY ARE ACAUSAL.  THIS IS A PROBLEM.  YOU SHOULD LOOK AT THE RESPONSES BEFORE INTERPOLATING
-    IN THE FREQUENCY DOMAIN, AND JUST DO THIS FROM THE TO-ZERO-MHZ VALUES.  THEN UPSAMPLE AFTER TO GET THE "INTERPOLATED"
-    VALUE. 
-
-
     This function uses code originally use in loadInterpolatedPhaseResponseMeanPreamp, but is seperated such that the
     system response (at least the portions we have measured) can be used seperately. 
 
@@ -44,6 +38,8 @@ def loadInterpolatedMeanResponse(mode, upsample_factor=16, plot=False):
     mode : str
         This determines which response to load.  Options: 'preamp', 'stage2', 'all'.  When all is selected, this function
         will call itself recursively and output all responses in the order listed in Options. 
+    return_domain : str
+        Either 'time' or 'freq', determines if the output is freq, response_fft or t, response
     upsample_factor : int
         This will be used to multiply the length of the time domain responses using scipy.signal.resample.  This should 
         be a factor of 2.  This is used to allow for better precision in the time domain aligning of signals before 
@@ -54,9 +50,12 @@ def loadInterpolatedMeanResponse(mode, upsample_factor=16, plot=False):
 
     Returns
     ----------
-    (mode_freq_Hz, mode_response_fft) or (modeA_freq_Hz, modeA_response_fft, modeB_freq_Hz, modeB_response_fft) if 
-    mode == 'all'.
-
+    If return_domain == 'freq' then:
+        (mode_freq_Hz, mode_response_fft) or (modeA_freq_Hz, modeA_response_fft, modeB_freq_Hz, modeB_response_fft) if 
+        mode == 'all'.
+    else:
+        (mode_t_s, mode_response) or (modeA_t_s, modeA_response, modeB_t_s, modeB_response) if 
+        mode == 'all'.
     '''
     try:
         if mode in ['preamp','stage2']:
@@ -274,18 +273,21 @@ def loadInterpolatedMeanResponse(mode, upsample_factor=16, plot=False):
             if plot == True:
                 plt.scatter(upsampled_time*1e9,averaged_upsampled_response/numpy.max(averaged_upsampled_response),label='Averaged Downsampled',s=20,c='r')
 
-            #Return Frequency domain response with original sampling.
-            out_response_fft = numpy.fft.rfft(downsampled_response)
-            out_freq_Hz = numpy.fft.rfftfreq(len(downsampled_time),downsampled_time[1]-downsampled_time[0])
+            if return_domain == 'time':
+                return downsampled_time, downsampled_response
+            else:
+                #Return Frequency domain response with original sampling.
+                out_response_fft = numpy.fft.rfft(downsampled_response)
+                out_freq_Hz = numpy.fft.rfftfreq(len(downsampled_time),downsampled_time[1]-downsampled_time[0])
 
-            return out_freq_Hz, out_response_fft
+                return out_freq_Hz, out_response_fft
 
             # Leave upsampling to another function?
         elif mode == 'all':
-            preamp_freq_Hz, preamp_response_fft = loadInterpolatedMeanResponse('preamp',plot=plot)
-            stage2_freq_Hz, stage2_response_fft = loadInterpolatedMeanResponse('stage2',plot=plot)
+            preamp_x, preamp_y = loadInterpolatedMeanResponse('preamp',plot=plot)
+            stage2_x, stage2_y = loadInterpolatedMeanResponse('stage2',plot=plot)
 
-            return preamp_freq_Hz, preamp_response_fft, stage2_freq_Hz, stage2_response_fft
+            return preamp_x, preamp_y, stage2_x, stage2_y
 
         else:
             print('Given mode not in list of acceptable modes.  Please give any of: "peamp", "stage2", or "all"')
@@ -333,10 +335,11 @@ class CosmicRayGenerator():
 
             #One-time preparation required for each model can be performed or called below.
             if self.model == 'bi-delta':
-                preamp_freq_Hz, preamp_response_fft, stage2_freq_Hz, stage2_response_fft = loadInterpolatedMeanResponse('all', plot=False)
-                self.preamp_response_fft = preamp_response_fft
-                self.stage2_response_fft = stage2_response_fft
-                self.response_freqs = preamp_response #Same for preamp and stage2
+                preamp_t_s, preamp_response, stage2_t_s, stage2_response = loadInterpolatedMeanResponse('all', return_domain='time', plot=False)
+                self.preamp_response = preamp_response
+                self.stage2_response = stage2_response
+                self.response_t_s = preamp_t_s #Same for preamp and stage2
+                self.response_t_ns = self.response_t_s*1.0e9
 
         except Exception as e:
             print('\nError in %s'%inspect.stack()[0][3])
@@ -346,7 +349,7 @@ class CosmicRayGenerator():
             print(exc_type, fname, exc_tb.tb_lineno)
         
 
-    def eFieldGenerator(self,t_ns,t_offset=0,extent_ns=5):
+    def eFieldGenerator(self,t_ns,t_offset=0,extent_ns=5,plot=False):
         '''
         For a given set of time data this will produce a signal for the set model.
 
@@ -362,21 +365,66 @@ class CosmicRayGenerator():
             How long the entire bipolar signal will take to return to no signal (roughly).  Only used in certain models
             (model == 'bi-delta').  Given in ns.
         '''
-        if self.model == 'bi-delta':
-            #Generate "Electric field" portion of bipolar signal (signal before response)
-            half_extent_index = int(numpy.ceil(extent_ns/(2*(t_ns[1] - t_ns[0])))) #The number of indices corresponding to half of the extent.  I.e. the extent in time (indices) of each pol of the bipolar signal.
-            print(half_extent_index)
-            efield = numpy.zeros_like(t_ns)
-            start_index = numpy.where(t_ns >= t_offset)[0][0]
-            efield[start_index:start_index+half_extent_index] = 1.0
-            efield[start_index + half_extent_index:start_index+2*half_extent_index] = -1.0
+        try:
+            if self.model == 'bi-delta':
+                #Generate "Electric field" portion of bipolar signal (signal before response)
+                half_extent_index = int(numpy.ceil(extent_ns/(2*(t_ns[1] - t_ns[0])))) #The number of indices corresponding to half of the extent.  I.e. the extent in time (indices) of each pol of the bipolar signal.
+                print(half_extent_index)
+                efield = numpy.zeros_like(t_ns)
+                start_index = numpy.where(t_ns >= t_offset)[0][0]
+                efield[start_index:start_index+half_extent_index] = 1.0
+                efield[start_index + half_extent_index:start_index+2*half_extent_index] = -1.0
 
-            #Use impulse response to get signal.
+                # self.preamp_response
+                # self.stage2_response
+                # self.response_t_s
 
-        else:
-            print('Model not yet programmed in function: eFieldGenerator')
+                #Compare time of response v.s. expected time.  Might be best to abstract this from the individual calling
+                #of the function and to put it in the setup.  Surely this is a stable request.
+                resample_factor = (self.response_t_ns[1] - self.response_t_ns[0])/(t_ns[1] - t_ns[0])
+                #If resample_factor > 1 then responses need to be upsampled by this factor to have the same time step.
+                #Less concerned about overall length, more concerned about matching time step for convolution.
+                stage2_response_resampled, response_t_s_resampled  = scipy.signal.resample(self.stage2_response,int(len(self.response_t_s)*resample_factor),t=self.response_t_s)
+                preamp_response_resampled, response_t_s_resampled  = scipy.signal.resample(self.preamp_response,int(len(self.response_t_s)*resample_factor),t=self.response_t_s)
 
-        return efield
+                #Use impulse response to get signal.
+                efield_convolved = numpy.convolve(numpy.convolve(efield,stage2_response_resampled,mode='same'),1.0e6*preamp_response_resampled,mode='same')
+                if len(response_t_s_resampled) > len(t_ns):
+                    #convolve mode 'same' will match max length of input array.  Can cut down the signal after matching.
+                    #should ultimately match len(t_ns)
+                    efield_convolved_t_ns = response_t_s_resampled*1e9
+                else:
+                    efield_convolved_t_ns = t_ns
+
+
+
+                #Plot efield signal.
+                if plot == True:
+                    plt.figure()
+                    
+                    plt.plot(response_t_s_resampled*1e9,stage2_response_resampled/numpy.max(stage2_response_resampled),label='stage2')
+                    plt.plot(response_t_s_resampled*1e9,preamp_response_resampled/numpy.max(preamp_response_resampled),label='preamp')
+                    plt.plot(t_ns,efield/numpy.max(efield),label='bipolar delta')
+            
+                    plt.plot(efield_convolved_t_ns,efield_convolved/numpy.max(efield_convolved),c='r',linestyle='--',label='Resultant Convolved "E Field" Signal')#Where the output convolved signal will be plotted.  
+
+                    plt.ylabel('Normalized Responses and Signals')
+                    plt.xlabel('ns')
+                    plt.legend()
+                    plt.minorticks_on()
+                    plt.grid(b=True, which='major', color='k', linestyle='-')
+                    plt.grid(b=True, which='minor', color='tab:gray', linestyle='--',alpha=0.5)
+
+            else:
+                print('Model not yet programmed in function: eFieldGenerator')
+
+            return efield
+        except Exception as e:
+            print('\nError in %s'%inspect.stack()[0][3])
+            print(e)
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
 
 
 
@@ -385,7 +433,6 @@ if __name__ == '__main__':
     try:
         plt.close('all')
         #Get timing info from real BEACON data for testing.
-        '''
         run = 1509
         known_pulser_ids = info.loadPulserEventids(remove_ignored=True)
         eventid = known_pulser_ids['run%i'%run]['hpol'][0]
@@ -394,13 +441,10 @@ if __name__ == '__main__':
         test_t = reader.t()
         test_pulser_adu = reader.wf(0)
 
-
-
-
         #Creating test signal
         cr_gen = CosmicRayGenerator(model='bi-delta')
 
-        test_e = cr_gen.eFieldGenerator(test_t,t_offset=500.0)
+        test_e = cr_gen.eFieldGenerator(test_t,t_offset=800.0,plot=True)
 
 
         plt.figure()
@@ -425,9 +469,6 @@ if __name__ == '__main__':
         plt.grid(b=True, which='major', color='k', linestyle='-')
         plt.grid(b=True, which='minor', color='tab:gray', linestyle='--',alpha=0.5)
 
-        '''
-
-        preamp_freq_Hz, preamp_response, stage2_freq_Hz, stage2_response = loadInterpolatedMeanResponse('all', plot=False)
 
     except Exception as e:
         print('Error in main loop.')
