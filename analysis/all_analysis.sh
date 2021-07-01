e9b99b8e1cc9161ea7ae427c461d69facc88dd2a
Dan Southall <dsouthall@uchicago.edu>
2021-07-01 14:03:45 -0500

    Updating to midway version.

--- analysis/all_analysis.sh
+++ analysis/all_analysis.sh
@@ -2,28 +2,28 @@
 # This will execute all of the current analysis scripts in turn for a given run number. 
 echo 'Executing analysis scripts for Run' $1  || exit
 
-# echo 'Attempting to prepare file by executing data_handler.py' || exit
-# python3 ${BEACON_ANALYSIS_DIR}tools/data_handler.py $1 || exit
+echo 'Attempting to prepare file by executing data_handler.py' || exit
+python3 ${BEACON_ANALYSIS_DIR}tools/data_handler.py $1 || exit
 
-# echo 'Attempting to prepare time delays by executing save_time_delays.py align_method = 0' || exit
-# python3 ${BEACON_ANALYSIS_DIR}analysis/save_time_delays.py $1 0 || exit
+echo 'Attempting to prepare time delays by executing save_time_delays.py align_method = 0' || exit
+python3 ${BEACON_ANALYSIS_DIR}analysis/save_time_delays.py $1 0 || exit
 
-# echo 'Attempting to identify cw by executing flag_cw.py' || exit
-# python3 ${BEACON_ANALYSIS_DIR}analysis/flag_cw.py $1 || exit
+echo 'Attempting to identify cw by executing flag_cw.py' || exit
+python3 ${BEACON_ANALYSIS_DIR}analysis/flag_cw.py $1 || exit
 
 # These need to be run after time delays. 
 
 # Depends on time delays to similar timings
-# echo 'Attempting to prepare counts for similarity between events by executing similarity.py' || exit
-# python3 ${BEACON_ANALYSIS_DIR}analysis/similarity.py $1 || exit
+echo 'Attempting to prepare counts for similarity between events by executing similarity.py' || exit
+python3 ${BEACON_ANALYSIS_DIR}analysis/similarity.py $1 || exit
 
 # Depends on time delays to align signals
-# echo 'Attempting to prepare impulsivity metric by executing impulsivity.py' || exit
-# python3 ${BEACON_ANALYSIS_DIR}analysis/impulsivity.py $1 || exit
+echo 'Attempting to prepare impulsivity metric by executing impulsivity.py' || exit
+python3 ${BEACON_ANALYSIS_DIR}analysis/impulsivity.py $1 || exit
 
 
-# echo 'Attempting to prepare simple CR template search executing simple_cr_template_search.py' || exit
-# python3 ${BEACON_ANALYSIS_DIR}analysis/cr_search/simple_cr_template_search.py $1 1 || exit
+echo 'Attempting to prepare simple CR template search executing simple_cr_template_search.py' || exit
+python3 ${BEACON_ANALYSIS_DIR}analysis/cr_search/simple_cr_template_search.py $1 1 || exit
 
 # This isn't super valuable until a good set of background templates is collected.  
 # echo 'Attempting to prepare impulsivity metric by executing correlate_with_background_templates.py' || exit
@@ -34,5 +34,5 @@ echo 'Executing analysis scripts for Run' $1  || exit
 
 # This is the correlation map script used for determining pointing direction.
 # NOTE: RF BG SEARCH IS TIME INSTENSIVE.  POSSIBLE THAT JOBS WILL BE CANCELLED BEFORE FOLLOWING SCRIPTS CAN FINISH.
-echo 'Attempting to prepare current pointing directions by executing rf_bg_search.py' || exit
-python3 ${BEACON_ANALYSIS_DIR}analysis/rf_bg_search.py $1 || exit
+# echo 'Attempting to prepare current pointing directions by executing rf_bg_search.py' || exit
+# python3 ${BEACON_ANALYSIS_DIR}analysis/rf_bg_search.py $1 || exit
--- tools/info.py
+++ tools/info.py
@@ -39,8 +39,8 @@ import matplotlib.pyplot as plt
 import scipy.interpolate
 
 #default_deploy = 30#os.path.join(os.environ['BEACON_ANALYSIS_DIR'], 'config/rtk-gps-day1-june20-2021.json')#30#2#13#19#15#The deployment calibration to use as the default.
-# default_deploy = os.path.join(os.environ['BEACON_ANALYSIS_DIR'], 'config/rtk-gps-day1-june20-2021.json')
-default_deploy = os.path.join(os.environ['BEACON_ANALYSIS_DIR'], 'config/rtk-gps-day3-june22-2021.json')
+default_deploy = os.path.join(os.environ['BEACON_ANALYSIS_DIR'], 'config/rtk-gps-day1-june20-2021.json')
+#default_deploy = os.path.join(os.environ['BEACON_ANALYSIS_DIR'], 'config/rtk-gps-day3-june22-2021.json')
 
 
 
